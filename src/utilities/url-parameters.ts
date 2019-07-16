318f3778a9c84c5882d723adb7db85278d145804
dalove1025 <dalove1025@gmail.com>
2019-07-15 22:34:32 -0400

    Url query parms to control size, wind, & spark

---
+++ src/utilities/url-parameters.ts
@@ -0,0 +1,49 @@
+import { parse } from "query-string";
+
+interface QueryParams {
+  simulationSize?: number[];
+  wind?: number;
+  spark?: number[];
+  spark2?: number[];
+}
+
+const defaultUrlParams: QueryParams = {
+  simulationSize: [9, 9],
+  wind: 88,
+  spark: [3, 6],
+  spark2: [-1, -1]
+};
+
+const urlParams: QueryParams = parse(location.search, {arrayFormat: "comma"});
+
+export function simulationSize(): number[] {
+  if (urlParams.simulationSize === undefined) {
+    return defaultUrlParams.simulationSize as number[];
+  } else {
+    return urlParams.simulationSize;
+  }
+}
+
+export function wind(): number {
+  if (urlParams.wind === undefined) {
+    return defaultUrlParams.wind as number;
+  } else {
+    return urlParams.wind;
+  }
+}
+
+export function spark(): number[] {
+  if (urlParams.spark === undefined) {
+    return defaultUrlParams.spark as number[];
+  } else {
+    return urlParams.spark;
+  }
+}
+
+export function spark2(): number[] {
+  if (urlParams.spark2 === undefined) {
+    return defaultUrlParams.spark2 as number[];
+  } else {
+    return urlParams.spark2;
+  }
+}
