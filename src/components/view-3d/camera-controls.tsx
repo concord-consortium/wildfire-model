81b66a231ec52f13b0de8d3f0458cbeff1bb9f05
pjanik <janikpiotrek@gmail.com>
2019-10-22 15:42:17 +0200

    Limit camera rotation
    [#169265877]

--- src/components/view-3d/camera-controls.tsx
+++ src/components/view-3d/camera-controls.tsx
@@ -10,6 +10,8 @@ export const CameraControls = () => {
     controls.minDistance = 0.8;
     controls.maxDistance = 3;
     controls.maxPolarAngle = Math.PI * 0.4; // don't let users look at the backside of the terrain
+    controls.minAzimuthAngle = -Math.PI * 0.25;
+    controls.maxAzimuthAngle = Math.PI * 0.25;
     controls.update();
   });
   return null;
