bc187a7173f99a36f19fbbfdd2326337077a6c7f
pjanik <janikpiotrek@gmail.com>
2019-10-22 11:13:15 +0200

    Refactor IconButton
    [#169016795]

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,46 +1,27 @@
-import { inject, observer } from "mobx-react";
 import * as React from "react";
-import { BaseComponent, IBaseProps } from "./base";
 import Button from "@material-ui/core/Button";
-
 import * as css from "./icon-button.scss";
 
-interface IProps extends IBaseProps {
-  icon: any;
-  highlightIcon: any;
-  buttonText: string;
-  onClick: any;
-  disabled: boolean;
+interface IProps {
+  icon: JSX.Element;
+  highlightIcon: JSX.Element;
+  buttonText?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  disabled?: boolean;
 }
 
-interface IState {}
-
-@inject("stores")
-@observer
-export class IconButton extends BaseComponent<IProps, IState> {
-  public render() {
-    const { icon, highlightIcon, onClick, disabled, buttonText } = this.props;
-    const { ui, simulation } = this.stores;
-    const uiDisabled = simulation.simulationStarted || ui.sparkPositionInteraction;
-    return (
-      <Button
-        onClick={onClick}
-        className={`${css.iconButton} ${disabled ? css.disabled : ""}`}
-        data-test="icon-button"
-        disableTouchRipple={true}
-        disabled={disabled}
-      >
+export const IconButton = ({ icon, highlightIcon, onClick, disabled, buttonText }: IProps) => (
+  <Button
+    onClick={onClick}
+    className={`${css.iconButton} ${disabled ? css.disabled : ""}`}
+    data-test="icon-button"
+    disableTouchRipple={true}
+    disabled={disabled}
+  >
         <span>
           <span className={css.iconButtonHighlightSvg}>{highlightIcon}</span>
           {icon}
           <span className={css.iconButtonText}>{buttonText}</span>
         </span>
-      </Button>
-    );
-  }
-
-  public showTerrainUI = () => {
-    const { ui } = this.stores;
-    ui.showTerrainUI = !ui.showTerrainUI;
-  }
-}
+  </Button>
+);
