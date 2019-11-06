360a57efddcbbbdacad09b82ac3b52db59a1be6e
Chris Hart <chart@concord.org>
2019-11-06 14:47:46 -0500

    Add icons for vegetation types

--- src/components/vegetation-selector.tsx
+++ src/components/vegetation-selector.tsx
@@ -3,6 +3,11 @@ import { Slider } from "@material-ui/core";
 import VerticalHandle from "../assets/slider-vertical.svg";
 import { TerrainType } from "../models/fire-model";
 
+import Grass from "../assets/terrain/vegetation-grass.svg";
+import Shrub from "../assets/terrain/vegetation-shrub.svg";
+import ForestSmallLitter from "../assets/terrain/vegetation-fsl.svg";
+import ForestLargeLitter from "../assets/terrain/vegetation-fll.svg";
+
 import * as css from "./vertical-selectors.scss";
 
 interface IProps {
@@ -40,15 +45,24 @@ const marks = [
   }
 ];
 
+const iconsMountains = [
+  <div className={`${css.sliderIcon} ${css.shrub} ${css.top} ${css.placeholder}`} key={0}><Shrub /></div>,
+  <div className={`${css.sliderIcon} ${css.fsl} ${css.mid} ${css.placeholder}`} key={1} ><ForestSmallLitter /></div>,
+  <div className={`${css.sliderIcon} ${css.fll} ${css.bottom} ${css.placeholder}`} key={2} ><ForestLargeLitter /></div>
+];
+const icons = [
+  <div className={`${css.sliderIcon} ${css.grass} ${css.top} ${css.placeholder}`} key={0}><Grass /></div>,
+  <div className={`${css.sliderIcon} ${css.shrub} ${css.mid} ${css.placeholder}`} key={1} ><Shrub /></div>,
+  <div className={`${css.sliderIcon} ${css.fsl} ${css.bottom} ${css.placeholder}`} key={2}><ForestSmallLitter /></div>
+];
+
 export const VegetationSelector = ({ vegetationType, terrainType, onChange }: IProps) =>
   (
     <div className={`${css.selector} ${css.vegetation}`}>
       <div className={css.header}>Vegetation Type</div>
       <div className={css.sliderContainer}>
         <div className={css.sliderIcons}>
-          <div className={`${css.sliderIcon} ${css.shrub} ${css.top} ${css.placeholder}`} />
-          <div className={`${css.sliderIcon} ${css.fsl} ${css.mid} ${css.placeholder}`} />
-          <div className={`${css.sliderIcon} ${css.fll} ${css.bottom} ${css.placeholder}`}/>
+          { terrainType === TerrainType.Mountains ? iconsMountains : icons }
         </div>
         <Slider
           classes={{ thumb: css.thumb, track: css.track, rail: css.rail, disabled: css.disabled }}
