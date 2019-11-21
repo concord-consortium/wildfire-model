bc052ac8d57ecb75678501d8df9ae0ff29f1534c
Chris Hart <chart@concord.org>
2019-11-21 11:41:45 -0500

    Add slider for wind speed

--- src/components/wind-circular-control.tsx
+++ src/components/wind-circular-control.tsx
@@ -1,32 +1,43 @@
-import React, { useEffect, useState, useRef } from "react";
-import { observer } from "mobx-react";
+import React, { useEffect, useState } from "react";
 import { useStores } from "../use-stores";
 import {
   CircularInput,
   CircularTrack,
-  CircularProgress,
-  CircularThumb,
-  useCircularInputContext,
-  useCircularDrag
 } from "react-circular-input";
 import WindDial from "../assets/wind-dial.svg";
 import WindArrow from "../assets/wind-arrow.svg";
 import WindSymbol from "../assets/wind-symbol.svg";
 import css from "./wind-circular-control.scss";
-import { CircularInputContext } from "react-circular-input/dist/CircularInputContext";
+import { Slider } from "@material-ui/core";
+import HorizontalHandle from "../assets/slider-horizontal.svg";
 
-// const handleMouse = (evt: MouseEvent) => {
-//   console.log(evt);
-// };
-
+const windSpeedMarks = [
+  {
+    value: 0,
+    label: "0"
+  },
+  {
+    value: 10,
+    label: "10"
+  },
+  {
+    value: 20,
+    label: "20"
+  },
+  {
+    value: 30,
+    label: "30"
+  }
+];
 export const WindCircularControl = () => {
   const { simulation, ui } = useStores();
-  const [directionAngle, setValue] = useState(simulation.wind.direction / 360);
+  const [directionAngle, setDirectionAngle] = useState(simulation.wind.direction / 360);
+  const [windSpeed, setWindSpeed] = useState(simulation.wind.speed);
 
   useEffect(() => {
     simulation.setWindDirection(angleToDirection());
-    simulation.setWindSpeed(10);
-  }, [directionAngle]);
+    simulation.setWindSpeed(windSpeed);
+  }, [directionAngle, windSpeed]);
 
   const angleToDirection = () => {
     // convert 0-1 scale of angle to the direction from which the wind is coming
@@ -43,6 +54,10 @@ export const WindCircularControl = () => {
     return arr[(val % 16)];
   };
 
+  const handleWindSpeedChange = (event: any, value: number | number[]) => {
+    setWindSpeed(value as number);
+  };
+
   return (
     <div className={css.windContainer}>
       <div className={css.controlContainer}>
@@ -52,13 +67,29 @@ export const WindCircularControl = () => {
         <div className={css.dialContainer}>
           <WindDial className={css.dial} />
           <WindArrow className={css.arrow} style={{transform: `rotate(${360 * directionAngle}deg)`}}/>
-          <CircularInput value={directionAngle} radius={35} onChange={setValue} className={css.windCircularControl}>
+          <CircularInput value={directionAngle} radius={35}
+            onChange={setDirectionAngle} className={css.windCircularControl}>
             <CircularTrack strokeWidth={4} stroke="rgba(255,255,255,0.5)" fill="rgba(255,255,255,0)" />
           </CircularInput>
         </div>
       </div>
+
       <div className={css.key}>Wind Direction and Speed</div>
-      <div className={css.windText}>{`30 MPH from the ${degToCompass()}`}</div>
+      <div className={css.windText}>{`${windSpeed} MPH from the ${degToCompass()}`}
+        <div className={css.windSliderControls}>
+          <Slider
+            classes={{ thumb: css.thumb, markLabel: css.markLabel }}
+            min={0}
+            max={30}
+            disabled={simulation.simulationStarted}
+            value={windSpeed}
+            step={1}
+            marks={windSpeedMarks}
+            onChange={handleWindSpeedChange}
+            ThumbComponent={HorizontalHandle}
+          />
+        </div>
+      </div>
     </div>
   );
 };
