adac477246f1b1b21a23e542ad8e141ff4814b5b
pjanik <janikpiotrek@gmail.com>
2020-03-13 00:22:39 +0100

    Add Shutterbug support
    [#171748812]

--- src/components/view-3d/view-3d.tsx
+++ src/components/view-3d/view-3d.tsx
@@ -1,8 +1,19 @@
-import React from "react";
-import { ThreeJSManager } from "../../react-three-hook/threejs-manager";
+import React, { useContext, useEffect } from "react";
+import { ThreeJSContext, ThreeJSManager } from "../../react-three-hook/threejs-manager";
 import { CameraControls } from "./camera-controls";
 import { Terrain } from "./terrain";
 import { Lights } from "./lights";
+import Shutterbug from "shutterbug";
+
+const ShutterbugSupport = React.memo(() => {
+  const { render } = useContext(ThreeJSContext);
+  (window as any).rend = render;
+  useEffect(() => {
+    Shutterbug.on("saycheese", render);
+    return () => Shutterbug.off("saycheese", render);
+  }, []);
+  return null;
+});
 
 // Note that React.memo is very important here. Let's try to limit number of unnecessary React re-renders to minimum.
 // Child components should subscribe to MobX store themselves, so they'll be re-rendered individually based on the
@@ -10,6 +21,7 @@ import { Lights } from "./lights";
 export const View3D = React.memo(() => {
   return (
     <ThreeJSManager>
+      <ShutterbugSupport/>
       <CameraControls/>
       <Lights/>
       <Terrain/>
