a2d8f7d8a4a6802af3f61a5477bfe3036ca7b430
pjanik <janikpiotrek@gmail.com>
2021-07-22 13:44:40 +0200

    Add fire intensity key Cypress test
    [#178742135]

---
+++ cypress/integration/url-params.ts
@@ -0,0 +1,14 @@
+context("Test URL params", () => {
+
+  context("showBurnIndex", () => {
+    it("doesn't render the fire intensity scale when ?showBurnIndex=false", () => {
+      cy.visit("/");
+      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.be.visible");
+    });
+
+    it("renders the fire intensity scale when ?showBurnIndex=true", () => {
+      cy.visit("/?showBurnIndex=true");
+      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("be.visible");
+    });
+  })
+});
