c6f9a68897cf71ea835a4d94d56cc61ad276401b
pjanik <janikpiotrek@gmail.com>
2021-07-22 23:42:14 +0200

    Address PR comment
    [#178951962]

--- cypress/integration/url-params.ts
+++ cypress/integration/url-params.ts
@@ -1,7 +1,7 @@
 context("Test URL params", () => {
 
   context("showBurnIndex", () => {
-    it("doesn't render the fire intensity scale when ?showBurnIndex=false", () => {
+    it("doesn't render the fire intensity scale when showBurnIndex URL param is omitted", () => {
       cy.visit("/");
       cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.exist");
     });
