8bbf7c7fb96ae6743fc4b7516833d6620e009a82
pjanik <janikpiotrek@gmail.com>
2021-07-22 13:47:54 +0200

    Update cypress
    [#178951962]

--- cypress/integration/url-params.ts
+++ cypress/integration/url-params.ts
@@ -3,7 +3,7 @@ context("Test URL params", () => {
   context("showBurnIndex", () => {
     it("doesn't render the fire intensity scale when ?showBurnIndex=false", () => {
       cy.visit("/");
-      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.be.visible");
+      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.exist");
     });
 
     it("renders the fire intensity scale when ?showBurnIndex=true", () => {
