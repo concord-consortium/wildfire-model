a9f6e8c5ec552107d935f65fb896f054e35ec23c
pjanik <janikpiotrek@gmail.com>
2021-08-04 11:59:24 +0200

    Update Cypress tests

--- cypress/integration/url-params.ts
+++ cypress/integration/url-params.ts
@@ -1,14 +1,14 @@
 context("Test URL params", () => {
 
   context("showBurnIndex", () => {
-    it("doesn't render the fire intensity scale when showBurnIndex URL param is omitted", () => {
+    it("renders the fire intensity scale when showBurnIndex URL param is omitted", () => {
       cy.visit("/");
-      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.exist");
+      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("be.visible");
     });
 
-    it("renders the fire intensity scale when ?showBurnIndex=true", () => {
-      cy.visit("/?showBurnIndex=true");
-      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("be.visible");
+    it("doesn't render the fire intensity scale when ?showBurnIndex=false", () => {
+      cy.visit("/?showBurnIndex=false");
+      cy.get(".fire-intensity-scale--fireIntensityScale--__wildfire-v1__").should("not.exist");
     });
   })
 });
