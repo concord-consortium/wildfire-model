ecf6a0ccf4eb00ca82921e0c371aa21dfcdbaa76
pjanik <janikpiotrek@gmail.com>
2023-05-30 13:09:55 +0200

    chore: update MobX configuration [PT-185278584]

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { Provider } from "mobx-react";
+import { configure } from "mobx";
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppComponent } from "./components/app";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { createStores } from "./models/stores";
 import hurricanesTheme from "./material-ui-theme";
+
+// Disable mobx strict mode. Make v6 compatible with v4/v5 that was not enforcing strict mode by default.
+configure({ enforceActions: "never" });
 
 const stores = createStores();
 
