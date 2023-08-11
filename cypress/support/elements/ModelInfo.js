491a4fe3b8bbcaf6979831cf5ddc209d54cd90f0
Saravanan Udayakumar <saravanan.udayakumar@tscube.com>
2023-08-11 21:27:55 +0530

    Wildfire Automation for Terrain Setup

--- cypress/support/elements/ModelInfo.js
+++ cypress/support/elements/ModelInfo.js
@@ -2,4 +2,10 @@ export class ModelInfo {
   getModelTimeProgress() {
     return cy.get(".app--timeDisplay--__wildfire-v1__");
   }
+  getZoneInfo() {
+    return cy.get("[data-testid=zone-info]");
+  }
+  getZoneName(index) {
+    return this.getZoneInfo().eq(index).find('.simulation-info--zoneName--__wildfire-v1__');
+  }
 }
